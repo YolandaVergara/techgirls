f0adced54f237f45e690047ae90a5d14430002f2
Isabel del R <i.beldelreal@gmail.com>
2020-02-01 17:02:34 +0100

    Update docs

---
+++ docs/precache-manifest.dae00cfe0fd1371d0a67709f8030c830.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2d16a50ff4597c12ee7d0e43925d05b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d76e7957151d1a590f94",
+    "url": "./static/css/main.58e5ee14.chunk.css"
+  },
+  {
+    "revision": "01f4b27cc49b0095e737",
+    "url": "./static/js/2.696d094b.chunk.js"
+  },
+  {
+    "revision": "0e59bfe8526dc0c1464815752a5226d0",
+    "url": "./static/js/2.696d094b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d76e7957151d1a590f94",
+    "url": "./static/js/main.02af6b35.chunk.js"
+  },
+  {
+    "revision": "6983486f1a659f6f9771",
+    "url": "./static/js/runtime-main.87aa16b9.js"
+  }
+]);
