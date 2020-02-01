c6b9eb76635cbf3570844d79432c2eb8029f980c
Yolanda <yv.avila@gmail.com>
2020-02-01 11:28:48 +0100

    add structure

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Fragment } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+
+    </Fragment>
+  );
+}
+
+export default App;
